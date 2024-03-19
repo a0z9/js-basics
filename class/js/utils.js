c60ec6fe6e6c00530eac250af9f768227c141a07
Andres <a0z9@rambler.ru>
2024-03-19 06:30:11 +0300

    Day2

--- class/js/utils.js
+++ class/js/utils.js
@@ -1,2 +1,20 @@
 
 const log = console.log;
+
+function mult(){
+	
+	const Ni=11; Nj=11;
+	let s1 = "";
+	for (let j=1; j<Nj; j++)
+		s1+="\t"+j;
+	log(s1);
+	for (let i=1; i<Ni; i++) {
+		s1 = i+"";
+		for (let j=1; j<Nj; j++) {
+			s1 += "\t" + (i*j);
+		}
+		log(s1);
+	}
+
+	
+}
