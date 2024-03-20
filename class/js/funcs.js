ea1b7ada4b06cfff4a141b80646f59b9b1d19a11
Andres <a0z9@rambler.ru>
2024-03-20 14:48:14 +0300

    funcs замыкания day3

--- class/js/funcs.js
+++ class/js/funcs.js
@@ -1,4 +1,4 @@
-
+'use strict';
 
 function sayHello(name='Peter'){
 	var привет = `Hello, ${name}!`;
@@ -8,8 +8,82 @@ function sayHello(name='Peter'){
 
 let sayHello2 =  function(name)
 {
-	log("Hi!");
+	name = name || 'Basil';
+	log(`Hi, ${name}!`);
 };
 
-let sayHello3 = (name)=>log("How do you do!");
+let sayHello3 = (name='Andrei')=>log(`${name}, how do you do!`);
+
+//области видимости
+let varGlobal = 'global';
+
+function F1(){
+	varGlobal = 'func F1';
+	log(`In func F1: ${varGlobal}`);
+}
+
+function F2()
+{
+	function F3(){
+	varGlobal = 'func F3';
+	log(`In func F3: ${varGlobal}`);
+	}
+
+	log(`In func F2: ${varGlobal}`);
+	
+	return F3();
+}
+
+var varGlobal2;
+
+function F4()
+{
+ var inner1 = 1;
+ let inner2 = 1;
+ var inner1 = "qqwe";
+ // let inner2 = [];  //Uncaught SyntaxError: redeclaration of let inner2
+ inner2 = [1,2,3];
+ varGlobal2 = "F4";	
+}
+
+{
+let inner = 1;	
+var inner2 = 10	
+}
+
+
+log(`In global: ${varGlobal}`);
+
+
+// замыкания
+
+const Counter1 = function(){
+	var counter = 0;
+	
+	function f1()
+	{
+		return ++counter;
+	}
+	return f1;  // f1() - not works!!
+}
+
+const Counter2 = function(counter = 0) {
+	
+	function f1()
+	{
+		return ++counter;
+	}
+	return f1;  // f1() - not works!!
+}
+
+const Counter = (c=0)=>()=>++c;
+ 
+
+
+
+
+
+
+
+
 
