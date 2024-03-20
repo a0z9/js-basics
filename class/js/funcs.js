f57784ae6ae3f892b192d74cb23e89866ea2190e
Andres <a0z9@rambler.ru>
2024-03-20 13:40:42 +0300

    funcs 1 day3

---
+++ class/js/funcs.js
@@ -0,0 +1,15 @@
+
+
+function sayHello(name='Peter'){
+	var привет = `Hello, ${name}!`;
+	log(привет);
+    return привет;
+}
+
+let sayHello2 =  function(name)
+{
+	log("Hi!");
+};
+
+let sayHello3 = (name)=>log("How do you do!");
+
