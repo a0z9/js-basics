a23f2e578900e666a52bc25be1dd2b2d066723bb
Andres <a0z9@rambler.ru>
2024-03-26 15:17:31 +0300

    final_day2

--- class/js/utils.js
+++ class/js/utils.js
@@ -1,1 +1,29 @@
 const log=console.log;
+
+
+const mult=()=>{
+	//    1  2  3  4 .. 10
+	// 1  1  2  3 ...
+	// 2  2  4  6 ...
+	// 3  3  6  9 ...
+	// ...
+	// 10
+
+	Ni=12; Nj=12;
+
+	let s = '';
+	for (let i=1; i<=Ni; i++)
+		s+='\t'+i;
+	log(s);
+
+	for (let j=1; j<=Nj; j++) {
+		s = j;
+		for (let i=1; i<=Ni; i++) {
+			s=s+'\t'+j*i;
+		}
+		log(s);
+	};
+
+	
+	
+}
